d9644472c3bdbec6508d954801d8f559c54d5023
joaomenna1 <joaomenna1@gmail.com>
2019-03-21 12:51:35 -0400

    ip

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -6,10 +6,7 @@ SECRET_KEY = ')340&61*&z%+(9rz(6byk#gn$yk4fj1ik&_uz@u3qx)1)g&vs4'
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-
-
-]
+ALLOWED_HOSTS = ['3.17.19.34']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -28,6 +25,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_celery_results',
+    'wordpress_api',
 ]
 
 MIDDLEWARE = [
--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^blog/', include('wordpress_api.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
